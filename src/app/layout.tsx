f6b0b4288c2c80684cc17faac9b162328d639877
merriem-amara <merriem.amara@gmail.com>
2026-08-25 14:56:31 +0500

    refactor: remove clerk authentication dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,8 +23,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <ClerkProvider>{children}</ClerkProvider>
+      <body className="min-h-full flex-col">
+        {children}
       </body>
     </html>
   );
